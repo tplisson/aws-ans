d517cd82b2cb3144db29bf0ce87f8cdfc8797a93
tom@saddlebacksolutions.com <tom@saddlebacksolutions.com>
2021-04-01 12:56:06 +0200

    Updates to the README file

--- nat-gateway/main.tf
+++ nat-gateway/main.tf
@@ -10,153 +10,140 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region     = "us-east-1"
+  region = "us-east-1"
 }
 
-# Configure a VPC in the 10.10/16 CIDR block
+# Configure a VPC in the 10.0/16 CIDR block
 resource "aws_vpc" "main" {
-  cidr_block       = "10.10.0.0/16"
+  cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
   tags = {
-    Name = "tom03"
+    Name = "nat-gw-demo"
   }
 }
 
-# Configure 3 subnets
+# Configure 2 subnets
 resource "aws_subnet" "cidr1" {
-  depends_on = [aws_vpc.main]
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.10.1.0/24"
+  depends_on        = [aws_vpc.main]
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.0.0/24"
   availability_zone = "us-east-1a"
   tags = {
-    Name = "tom03-cidr-1"
+    Name = "nat-gw-demo-cidr-1"
   }
 }
 resource "aws_subnet" "cidr2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.10.2.0/24"
-  availability_zone = "us-east-1b"
+  depends_on        = [aws_vpc.main]
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
   tags = {
-    Name = "tom03-cidr-2"
-  }
-}
-resource "aws_subnet" "cidr3" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.10.3.0/24"
-  availability_zone = "us-east-1c"
-  tags = {
-    Name = "tom03-cidr-3"
+    Name = "nat-gw-demo-cidr-2"
   }
 }
 
 # Configure an IGW
-resource "aws_internet_gateway" "igw1" {
+resource "aws_internet_gateway" "igw" {
   depends_on = [
     aws_vpc.main,
     aws_subnet.cidr1,
-    aws_subnet.cidr2,
-    aws_subnet.cidr3
+    aws_subnet.cidr2
   ]
   vpc_id = aws_vpc.main.id
   tags = {
-    Name = "tom03-igw1"
+    Name = "nat-gw-demo-igw"
   }
 }
 
 # Configure default routes
-resource "aws_route_table" "rt1" {
+resource "aws_route_table" "rt-igw" {
   depends_on = [
     aws_vpc.main,
-    aws_internet_gateway.igw1
+    aws_internet_gateway.igw
   ]
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw1.id
+    gateway_id = aws_internet_gateway.igw.id
   }
   tags = {
-    Name = "tom03-rt-igw1-0/0"
+    Name = "nat-gw-demo-rt-igw-0/0"
   }
 }
 
 # Configre an Elastic IP for the NAT Gateway
-resource "aws_eip" "nat1" {
+resource "aws_eip" "eip1" {
   vpc = true
   tags = {
-    Name = "tom03-nat-gw-eip-1"
+    Name = "nat-gw-demo-nat-gw-eip1"
   }
 }
 
 # Configure a NAT Gateway
-resource "aws_nat_gateway" "natgw1" {
-  allocation_id = aws_eip.nat1.id
+resource "aws_nat_gateway" "natgw" {
+  depends_on    = [aws_internet_gateway.igw]
+  allocation_id = aws_eip.eip1.id
   subnet_id     = aws_subnet.cidr1.id
-  depends_on    = [aws_internet_gateway.igw1]
-  tags  = {
-    Name = "tom03-nat-gw1"
+  tags = {
+    Name = "nat-gw-demo-nat-gw"
   }
 }
 
 # Configuring a Route Table for the NAT Gateway
-resource "aws_route_table" "rt-nat1" {
+resource "aws_route_table" "rt-nat" {
   depends_on = [
-    aws_nat_gateway.natgw1
+    aws_nat_gateway.natgw
   ]
   vpc_id = aws_vpc.main.id
   route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.natgw1.id
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.natgw.id
   }
   tags = {
-    Name = "tom03-rt-nat1-0/0"
+    Name = "nat-gw-demo-rt-nat-0/0"
   }
 }
 
 # Creating an Route Table Association of the NAT Gateway route table with the Private Subnet!
-resource "aws_route_table_association" "natgw1-assoc" {
+resource "aws_route_table_association" "natgw-assoc" {
   depends_on = [
-    aws_route_table.rt-nat1
+    aws_route_table.rt-nat
   ]
-#  Private Subnet ID for adding this route table to the DHCP server of Private subnet!
-  subnet_id      = aws_subnet.cidr1.id
-# Route Table ID
-  route_table_id = aws_route_table.rt-nat1.id
-}
-
-
-# Configure an EC2 instance for VM1
-resource "aws_instance" "vm1" {
-  depends_on = [ aws_subnet.cidr1 ]
-  ami           = data.aws_ami.latest-ubuntu.id
-  instance_type = "t2.micro"
+  #  Private Subnet ID for adding this route table to the DHCP server of Private subnet!
   subnet_id = aws_subnet.cidr1.id
-  private_ip = "10.10.1.10"
+  # Route Table ID
+  route_table_id = aws_route_table.rt-nat.id
+}
+
+# Configuring an EC2 instance for a web server in the public subnet
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.latest-ubuntu.id
+  instance_type = "t2.nano"
+  subnet_id     = aws_subnet.cidr1.id
+  private_ip    = "10.0.0.10"
+
   tags = {
-    Name = "tom03-vm1"
+    Name = "nat-gw-demo-ec2-web"
   }
 }
 
-# Configure an EC2 instance for VM2
-resource "aws_instance" "vm2" {
-  depends_on = [ aws_subnet.cidr2 ]
-  ami           = data.aws_ami.latest-ubuntu.id
-  instance_type = "t2.micro"
-  subnet_id 	= aws_subnet.cidr2.id
-  private_ip 	= "10.10.2.10"
-  tags = {
-    Name = "tom03-vm2"
+# Configuring 3x EC2 instances for DB servers in the private subnet
+variable "instance_ips" {
+  default = {
+    "0" = "10.0.1.11"
+    "1" = "10.0.1.12"
+    "2" = "10.0.1.13"
   }
 }
-
-# Configure an EC2 instance for VM3
-resource "aws_instance" "vm3" {
-  depends_on = [ aws_subnet.cidr3 ]
+resource "aws_instance" "db" {
+  count         = "3"
   ami           = data.aws_ami.latest-ubuntu.id
-  instance_type = "t2.micro"
-  subnet_id 	= aws_subnet.cidr3.id
-  private_ip 	= "10.10.3.10"
+  instance_type = "t2.nano"
+  subnet_id     = aws_subnet.cidr2.id
+  private_ip    = lookup(var.instance_ips, count.index)
+
   tags = {
-    Name = "tom03-vm3"
+    Name = "nat-gw-demo-ec2-db${count.index + 1}"
   }
 }
 
