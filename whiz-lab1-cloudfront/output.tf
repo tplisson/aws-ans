2715277b1693b99c07337684b8a1f648f14b23b6
tom@saddlebacksolutions.com <tom@saddlebacksolutions.com>
2021-04-30 19:16:16 +0200

    Adding the Whizlabs #1

--- whiz-lab1-cloudfront/output.tf
+++
@@ -1,4 +0,0 @@
-output "aws_cloudfront_distribution" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_cloudfront_distribution.s3_distribution.domain_name
-}
--- whiz-lab1-cloudfront/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "AWS-REGION" {
-  description = "AWS Region"
-  default     = "us-east-1"
-}
-variable "S3-BUCKET" {
-  description = "Unique S3 bucket name"
-  default     = "whizlabs-tplisson-lab1-04"
-}
