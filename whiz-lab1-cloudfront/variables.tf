94164f5818f7a2c08472bb2d406b8c7ff55442db
tom@saddlebacksolutions.com <tom@saddlebacksolutions.com>
2021-04-30 19:16:32 +0200

    Adding the Whizlabs #1

---
+++ whiz-lab1-cloudfront/variables.tf
@@ -0,0 +1,8 @@
+variable "AWS-REGION" {
+  description = "AWS Region"
+  default     = "us-east-1"
+}
+variable "S3-BUCKET" {
+  description = "Unique S3 bucket name"
+  default     = "whizlabs-tplisson-lab1-04"
+}
